40379e4ad29b55774dbbedc3a956c436c8bcf3a5
TimotheeClear <89906603+TimotheeClear@users.noreply.github.com>
2022-02-14 17:55:30 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ktr_msc_ss1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
